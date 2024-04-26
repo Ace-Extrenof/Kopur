025a8093f3c072c633b8eeb7967366576a19b47d
Ace-Extrenof <prateeshAce@proton.me>
2024-04-26 20:11:25 +0530

    batman

---
+++ README.md
@@ -0,0 +1,5 @@
+# Study Helper
+The perfect study tool for terminal enthusiasts (why would anyone want to study tho?)
+
+## Installation
+soon :)
---
+++ main.py
@@ -0,0 +1,32 @@
+import sys
+import colorama
+import os
+from colorama import just_fix_windows_console
+just_fix_windows_console()
+colorama.init()
+
+def title_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    print(colorama.Fore.BLUE + "##### STOODY HELPER ######\n\n-----1) KANBAN-----\n-----2) TASK MANAGER")
+    global inp 
+    inp = input("\uf061 ")
+    return inp
+
+def options(INPUT):
+    if INPUT == "KANBAN".lower() or '1' or '1)':
+        kanban()
+    elif INPUT == "TASK MANAGER" or '2' or '2)':
+        task_manager()
+    else:
+        print("err... \nNOT AN OPTION")
+
+def kanban():
+    print("kanban ;)")
+
+def task_manager():
+    print("task manager")
+
+if __name__ == "__main__":
+    title_screen()
+    options(inp)
